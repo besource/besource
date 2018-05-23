a32beb2ed8c47f6f1de65b408c49735e89c48718
besource <39544522+besource@users.noreply.github.com>
2018-05-23 14:10:18 +0900

    cleansing

--- CPP/40.Information_Leak_of_System_Data/C40001.cpp
+++
@@ -1,9 +0,0 @@
-#include "std_testcase.h"
-#include <stdio.h>
-
-void cwe497_bad()
-{
-    char* path = getenv("PATH");
-    /*  */
-    sprintf(stderr, "cannot find exe on path %s\n", path);    
-}
